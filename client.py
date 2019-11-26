ed71f8230b6c1e9108e4e619b7f495bba28f131d
huantingwei <ting02940@gmail.com>
2019-11-26 12:04:38 +0100

    part 2-buffering and streaming

--- client.py
+++ client.py
@@ -1,13 +1,27 @@
 import socket
 
+HEADER_SIZE = 10
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
 
-full_msg = ''
 while True:
-    msg = s.recv(8) # buffer in bytes    
-    if len(msg) <= 0:
-        break
-    full_msg += msg.decode("utf-8")
-print(full_msg)
+
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(16) # buffer in bytes    
+        if new_msg:
+            print(f"new message length: {msg[:HEADER_SIZE]}")
+            msglen = int(msg[:HEADER_SIZE])
+            new_msg = False
+
+        full_msg += msg.decode("utf-8")
+
+        if len(full_msg) - HEADER_SIZE == msglen:
+            print("full msg recvd")
+            print(full_msg[HEADER_SIZE:])
+            new_msg = True
+            full_msg = ''
+    print(full_msg)
     
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 # socket: endpoints that send/receive data
 import socket
+import time
+
+HEADER_SIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
@@ -7,6 +10,17 @@ s.listen(5)
 
 while True:
     clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established!")   
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
-    clientsocket.close()
+    print(f"Connection from {address} has been established!")
+
+    msg = "Welcome to the server!"
+    msg = f'{len(msg):<{HEADER_SIZE}}' + msg
+
+    clientsocket.send(bytes(msg, "utf-8"))
+    # clientsocket.close()
+
+    while True:
+        time.sleep(3)
+        msg = f"The time is! {time.time()}"
+        msg = f'{len(msg):<{HEADER_SIZE}}' + msg
+        clientsocket.send(bytes(msg, "utf-8"))
+    
